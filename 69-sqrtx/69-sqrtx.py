9012edf3779f12803114f740fe48531927c68f5b
Emilcan Arıcan <emilcan.arican@boun.edu.tr>
2022-10-10 02:07:35 +0300

    Time: 66 ms (58.39%), Space: 14 MB (11.06%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        start, end = 0, 65536
+        
+        while start <= end:
+            mid = (start + end) // 2
+
+            if mid * mid <= x:
+                start = mid + 1
+            else:
+                end = mid - 1
+        
+        return start - 1
