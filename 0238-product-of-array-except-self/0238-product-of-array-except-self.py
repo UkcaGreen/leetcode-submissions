42c63354ea0051ce62b13d79f9fa11b377ae59d0
Emilcan Arıcan <emilcan.arican@gmail.com>
2023-10-30 19:22:11 +0300

    Time: 190 ms (73.91%), Space: 23.8 MB (74.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,16 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:    
         
-        zero_count = len([n for n in nums if n == 0])
+        from functools import reduce
+
+        zero_count = nums.count(0)
         
-        prod_of_all_nums = reduce(lambda a, b: a * b, nums)
+        if zero_count > 1:
+            return [0 for n in nums]
         
-        if zero_count == 0:
-            return [prod_of_all_nums // n for n in nums]
+        non_zero_nums = [n for n in nums if n != 0]
+        
+        non_zero_product = 1
+        
+        for n in non_zero_nums:
+            non_zero_product *= n
+        
         if zero_count == 1:
-            prod_of_all_nums_except_zero = reduce(lambda a, b: a * b, [n for n in nums if n != 0])
-            return [0 if n != 0 else prod_of_all_nums_except_zero for n in nums]
-        if zero_count >= 2:
-            return [0 for _ in nums]
+            return [0 if n != 0 else non_zero_product for n in nums]
+        
+        return [non_zero_product // n for n in nums]
+    
+
             
         
