ba648836fdf42c610257a85e37bd9842db603678
Emilcan Arıcan <emilcan.arican@gmail.com>
2023-10-30 18:25:11 +0300

    Time: 120 ms (11.47%), Space: 24.7 MB (11.80%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = {}
-
-        for s in strs:
-
-            key = "".join(sorted(s))
-
-            if key not in ans:
-                ans[key] = []
-
-            ans[key].append(s)
         
-        return list(ans.values())
+        from collections import Counter, defaultdict
+        
+        groups = defaultdict(lambda: [])
+        
+        
+        for s in strs:
+            
+            groups[tuple(sorted(Counter(s).items()))].append(s)
+            
+        
+        return groups.values()
+            
