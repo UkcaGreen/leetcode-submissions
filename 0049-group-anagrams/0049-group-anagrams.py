a7423c52b78d929505a3b4997e007c91392e4b54
Emilcan Arıcan <emilcan.arican@gmail.com>
2023-10-30 18:51:35 +0300

    Time: 114 ms (17.86%), Space: 22.2 MB (21.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,11 +5,17 @@ class Solution:
         
         groups = defaultdict(lambda: [])
         
-        
         for s in strs:
             
-            groups[tuple(sorted(Counter(s).items()))].append(s)
+            char_counts = [0] * 26
             
+            for c in s:
+                
+                idx = ord(c) - ord("a")
+        
+                char_counts[idx] += 1
+            
+            groups[tuple(char_counts)].append(s)
         
         return groups.values()
             
