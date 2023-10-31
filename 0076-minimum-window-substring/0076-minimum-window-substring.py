da21627ac8cedb47ac8fb9c5fcba227e9bf73a1d
Emilcan Arıcan <emilcan.arican@gmail.com>
2023-10-31 23:55:48 +0300

    Time: 865 ms (5.01%), Space: 18.2 MB (5.00%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -4,8 +4,9 @@ class Solution:
         from collections import Counter, defaultdict
         
         target_counts = Counter(t)
-        
         counts = defaultdict(lambda: 0)
+        
+        is_met = {k: False for k in target_counts}
         
         l, r = 0, 0
         
@@ -15,12 +16,20 @@ class Solution:
             
             sub_str = s[l:r]
             
-            if any(counts[k] < target_counts[k] for k in target_counts):
+            any(counts[k] < target_counts[k] for k in target_counts)
+            
+            if not all(is_met.values()):
                 
                 r += 1
                 
                 if r <= len(s):
-                    counts[s[r-1]] += 1
+                    
+                    new_char = s[r-1]
+                    
+                    counts[new_char] += 1
+                    
+                    if new_char in target_counts and counts[new_char] >= target_counts[new_char]:
+                        is_met[new_char] = True
             else:
                 
                 if len(sub_str) == 0:
@@ -28,8 +37,14 @@ class Solution:
                     
                 if len(sub_str) < len(ans):
                     ans = sub_str
+                    
+                old_char = s[l]
                 
-                counts[s[l]] -= 1
+                counts[old_char] -= 1
+                
+                if old_char in target_counts and counts[old_char] < target_counts[old_char]:
+                    is_met[old_char] = False
+                
                 l += 1
                 
         if ans == " " * int(10e5):
