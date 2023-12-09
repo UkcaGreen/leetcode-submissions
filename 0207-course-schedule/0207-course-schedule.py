45aa7c87ed7578bd64934bd434066896125cf097
Emilcan Arıcan <emilcan.arican@gmail.com>
2023-12-10 02:00:18 +0300

    Time: 319 ms (5.02%), Space: 18 MB (65.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -15,26 +15,7 @@ class Solution:
         
         
         return True
-        
-        def dfs(n, s):
-            
-            if n in s:
-                return False
-            
-            neighbors = [e[1] for e in prerequisites if e[0] == n]
-            
-            result = True
-            
-            for neighbor in neighbors:
-                s.append(n)
-                result = result and dfs(neighbor, s)
-                s.pop()
-            
-            return result
-        
-        x = {e[0] for e in prerequisites}
-    
-        return all(dfs(i, []) for i in x)
+
             
             
             
