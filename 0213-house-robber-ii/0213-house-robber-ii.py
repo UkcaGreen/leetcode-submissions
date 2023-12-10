b411c574b3f1cb5c3b9620b40399867616bfbbee
Emilcan Arıcan <emilcan.arican@gmail.com>
2023-12-10 05:19:29 +0300

    Time: 49 ms (6.56%), Space: 16.6 MB (6.60%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,40 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        if len(nums) == 1:
+            return nums[0]
+        
         from functools import cache
         
         @cache
-        def dfs(house_idx, is_first_one_robbed, is_prev_robbed=False):
+        def dfs(house_idx, last_idx, is_prev_robbed=False):
             
-            if house_idx == len(nums):
+            if house_idx == last_idx + 1:
                 return 0
-        
-            if house_idx == 0:
-                return max(
-                    dfs(house_idx + 1, True, True) + nums[house_idx],
-                    dfs(house_idx + 1, False, False),
-                )
-            
-            if house_idx == len(nums) - 1:
-                
-                if is_first_one_robbed:
-                    return 0
-                
-                if is_prev_robbed:
-                    return dfs(house_idx + 1, is_first_one_robbed, False)
-                else:
-                    return max(
-                        dfs(house_idx + 1, is_first_one_robbed, True) + nums[house_idx],
-                        dfs(house_idx + 1, is_first_one_robbed, False),
-                    )
-                    
             
             if is_prev_robbed:
-                return dfs(house_idx + 1, is_first_one_robbed, False)
+                return dfs(house_idx + 1, last_idx, False)
             else:
                 return max(
-                    dfs(house_idx + 1, is_first_one_robbed, True) + nums[house_idx],
-                    dfs(house_idx + 1, is_first_one_robbed, False),
+                    dfs(house_idx + 1, last_idx, True) + nums[house_idx],
+                    dfs(house_idx + 1, last_idx, False),
                 )
             
-        return dfs(0, None)
+        return max(
+            dfs(1, len(nums) - 1),
+            dfs(0, len(nums) - 2),
+        )
